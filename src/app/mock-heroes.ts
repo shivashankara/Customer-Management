520502b953920c8ebdb82c1ef65c577e160304cc
shivashankara <shivu.murthy1@gmail.com>
2017-12-26 14:38:39 +0530

    Pull latest code from github

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] =[
+    {id:1,name:'Ali'},
+    {id:1,name:'Fareen'},
+    {id:1,name:'Anil'},
+    {id:1,name:'Manju'},
+    {id:1,name:'vishnu'},
+    {id:1,name:'ritesh'},
+    {id:1,name:'rajesh'},
+]
